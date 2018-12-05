7c8d08e309e2b15e88f7276ed28ef150cb972c68
Anastasia876445609 <876445609@qq.com>
2018-12-06 00:19:05 +0800

    提交一次

--- idearesp/src/main/java/com/atguigu/ClassTest.java
+++ idearesp/src/main/java/com/atguigu/ClassTest.java
@@ -6,7 +6,7 @@ package com.atguigu;
 public class ClassTest {
     public static void main(String[] args) {
         System.out.println("BBBBBBBBBBBBBBB");
-        System.out.println("AAAAAAAAAAAAA");
+        System.out.println("AAAAAAAAAAAAAA");
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
