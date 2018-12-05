81f5103ae460f1c2d276ce6a233a26c247db5717
openmrliu <53160569@qq.com>
2018-12-05 23:33:09 +0800

    open submit

--- idearesp/src/main/java/com/atguigu/ClassTest.java
+++ idearesp/src/main/java/com/atguigu/ClassTest.java
@@ -5,7 +5,7 @@ package com.atguigu;
  */
 public class ClassTest {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("111111111111111111");
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
