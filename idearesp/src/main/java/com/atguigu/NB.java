36ea236107622c96b70dbf6214fb5dc039d0aca3
openmrliu <53160569@qq.com>
2018-12-06 00:12:30 +0800

    new Branch

---
+++ idearesp/src/main/java/com/atguigu/NB.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+/**
+ * Created by openMrliu on 2018/12/6.
+ */
+public class NB {
+    public static void main(String[] args) {
+        System.out.println("nb");
+    }
+}
