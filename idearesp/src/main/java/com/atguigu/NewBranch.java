d521fc879073e72fb439a02f2c3508fffe069622
openmrliu <53160569@qq.com>
2018-12-06 00:00:04 +0800

    new Branch

---
+++ idearesp/src/main/java/com/atguigu/NewBranch.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+/**
+ * Created by openMrliu on 2018/12/5.
+ */
+public class NewBranch {
+    public static void main(String[] args) {
+        System.out.println("new Branch");
+    }
+}
