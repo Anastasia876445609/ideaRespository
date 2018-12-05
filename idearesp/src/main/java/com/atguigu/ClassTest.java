ed80bb1eba3988c1d10d37b8b6a1fa0fdcb3c26b
openmrliu <openkk@123.com>
2018-12-05 21:12:43 +0800

    提交一次

---
+++ idearesp/src/main/java/com/atguigu/ClassTest.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+/**
+ * Created by openMrliu on 2018/12/5.
+ */
+public class ClassTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
